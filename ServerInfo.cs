a37ef992bb84c9aa49e7ad0531fa12eb036cbba7
Stimayk <51941742+Stimayk@users.noreply.github.com>
2024-01-06 12:30:11 +0400

    Add files via upload

--- ServerInfo.cs
+++ ServerInfo.cs
@@ -5,6 +5,8 @@ using System.Text.RegularExpressions;
 using System.Text;
 using CounterStrikeSharp.API.Modules.Timers;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
+using MySqlConnector;
+using Newtonsoft.Json;
 
 namespace ServerInfo
 {
@@ -12,7 +14,7 @@ namespace ServerInfo
     {
         public override string ModuleName => "ServerInfo for LR WEB";
         public override string ModuleAuthor => "E!N";
-        public override string ModuleVersion => "1.1";
+        public override string ModuleVersion => "1.2";
 
         private const int MaxPlayers = 65;
         private string server = "";
@@ -36,6 +38,10 @@ namespace ServerInfo
                 AddTimer(1.0f, () =>
                 {
                     InitPlayers(player);
+                    if (PlayerList.TryGetValue(player.Slot, out var playerInfo))
+                    {
+                        playerInfo.SessionStartTime = DateTime.Now;
+                    }
                 });
             });
         }
@@ -110,16 +116,52 @@ namespace ServerInfo
             return $"{serverIp}:{serverPort}";
         }
 
+        public int GetRankFromDatabase(string? steamid)
+        {
+            DirectoryInfo? moduleDirectoryParent = Directory.GetParent(ModuleDirectory) ?? throw new InvalidOperationException($"Module directory parent is null");
+            DirectoryInfo? parentDirectory = moduleDirectoryParent.Parent ?? throw new InvalidOperationException($"Parent directory is null");
+            string configFilePath = Path.Combine(parentDirectory.FullName, "plugins/RanksPoints/dbconfig.json");
+            var configFileContent = File.ReadAllText(configFilePath);
+            var dbConfig = JsonConvert.DeserializeObject<DbConfig>(configFileContent);
+            if (dbConfig != null)
+            {
+                var connectionString = new MySqlConnectionStringBuilder
+                {
+                    Server = dbConfig.DbHost,
+                    UserID = dbConfig.DbUser,
+                    Password = dbConfig.DbPassword,
+                    Database = dbConfig.DbName,
+                    Port = uint.Parse(dbConfig.DbPort),
+                }.ToString();
+
+                using var connection = new MySqlConnection(connectionString);
+                try
+                {
+                    connection.Open();
+
+                    var query = "SELECT rank FROM lvl_base WHERE steam = @SteamId";
+                    using var command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@SteamId", steamid);
+                    var result = command.ExecuteScalar();
+                    if (result != null)
+                    {
+                        return Convert.ToInt32(result);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error when connecting to the database: " + ex.Message);
+                }
+            }
+            return 0;
+        }
+
         [GameEventHandler]
         private HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
         {
             CCSPlayerController _player = @event.Userid;
-
             if (!IsPlayerValid(_player)) return HookResult.Continue;
-            if (PlayerList.TryGetValue(_player.Slot, out var theplayer))
-            {
-                PlayerList.Remove(_player.Slot);
-            }
+            PlayerList.Remove(_player.Slot);
             return HookResult.Continue;
         }
 
@@ -127,10 +169,17 @@ namespace ServerInfo
         {
             if (!IsPlayerValid(player)) return;
 
+            string? steamId2 = null;
+            if (player.AuthorizedSteamID != null)
+            {
+                steamId2 = ConvertToSteam2((long)player.AuthorizedSteamID.SteamId64);
+            }
+
             PlayerInfo playerInfo = new()
             {
                 UserId = player.UserId,
                 SteamId = player.AuthorizedSteamID?.SteamId64.ToString(),
+                SteamId2 = steamId2,
                 Name = player.PlayerName,
                 Kills = player.ActionTrackingServices?.MatchStats.Kills ?? 0,
                 Deaths = player.ActionTrackingServices?.MatchStats.Deaths ?? 0,
@@ -139,6 +188,21 @@ namespace ServerInfo
             };
 
             PlayerList[player.Slot] = playerInfo;
+        }
+
+        private string ConvertToSteam2(long steamId64)
+        {
+            long steamId3 = steamId64 - 76561197960265728;
+            int y = (int)(steamId3 % 2);
+            long z = steamId3 / 2;
+
+            return $"STEAM_1:{y}:{z}";
+        }
+
+        private int CalculatePlayTime(PlayerInfo playerInfo)
+        {
+            TimeSpan timeSpent = DateTime.Now - playerInfo.SessionStartTime;
+            return (int)timeSpent.TotalSeconds;
         }
 
         private void UpdatePlayerInfo()
@@ -182,11 +246,13 @@ namespace ServerInfo
             var (t1score, t2score) = GetTeamsScore();
             var jsonData = new { score_ct = t1score, score_t = t2score, players = jsonPlayers };
             var jsonString = System.Text.Json.JsonSerializer.Serialize(jsonData);
+
             PostData(jsonString);
         }
 
-        private static List<object> GetPlayersJson(PlayerInfo playerinfo)
+        private List<object> GetPlayersJson(PlayerInfo playerinfo)
         {
+            var playTime = CalculatePlayTime(playerinfo);
             var jsonPlayers = new List<object>
             {
                 new
@@ -197,8 +263,8 @@ namespace ServerInfo
                     assists = playerinfo.Deaths.ToString(),
                     death = playerinfo.Assists.ToString(),
                     headshots = playerinfo.Headshots.ToString(),
-                    rank = "0",
-                    playtime = 0
+                    rank = GetRankFromDatabase(playerinfo.SteamId2),
+                    playtime = playTime
                 }
             };
             return jsonPlayers;
@@ -219,12 +285,12 @@ namespace ServerInfo
 
                 if (!response.IsSuccessStatusCode)
                 {
-                    Server.PrintToConsole("Ошибка при отправке запроса.");
+                    Console.WriteLine("Error when sending a request.");
                 }
             }
             catch (Exception ex)
             {
-                Server.PrintToConsole($"Exception in HTTP request: {ex.Message}");
+                Console.WriteLine($"Exception in HTTP request: {ex.Message}");
             }
 
         }
@@ -245,10 +311,22 @@ namespace ServerInfo
     {
         public int? UserId { get; set; }
         public string? SteamId { get; set; }
+        public string? SteamId2 { get; set; }
         public string? Name { get; set; }
         public int? Kills { get; set; } = 0;
         public int? Deaths { get; set; } = 0;
         public int? Assists { get; set; } = 0;
         public int? Headshots { get; set; } = 0;
+        public DateTime SessionStartTime { get; set; }
+    }
+
+    public class DbConfig
+    {
+        public required string DbHost { get; set; }
+        public required string DbUser { get; set; }
+        public required string DbPassword { get; set; }
+        public required string DbName { get; set; }
+        public required string DbPort { get; set; }
+        public required string Name { get; set; }
     }
 }
